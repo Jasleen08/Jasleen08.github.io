c69716b7c75bd3af98cb33c11ea07d47ba74f7f8
Jasleen08 <109775919+Jasleen08@users.noreply.github.com>
2022-07-21 22:49:24 -0400

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+/* .js files add interaction to your website */
+
+var displayName = document.getElementById("secret");
+var button = document.getElementById("myButton");
+
+  button.addEventListener("click", displaySecret);
+
+
+
+function displaySecret(){
+  var text = document.getElementById("myText").value;
+  displayName.innerHTML = text + ", we need to encourage girls that their voice matters. I think there are hundreds and thousands of Malalas out there."
+}
